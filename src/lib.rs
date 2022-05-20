2d9da3b8afce0c9ad90c32847cb8f276d8b78d53
Timothy248 <tbenker@gmx.ch>
2022-05-20 21:19:57 +0200

    First working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,42 @@
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
+use std::ops::{AddAssign, SubAssign};
+use std::sync::atomic::AtomicUsize;
 use std::thread;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock, Condvar, Mutex};
+use std::time::Duration;
 
-pub struct Scatter<T, S> {
-    area: usize, // thread count
+pub struct Scatter<S: 'static + Send, T: 'static + Send> {
+    area: usize, // max thread count
     function: fn(T) -> S,
+    values: Arc<RwLock<HashMap<usize, S>>>,
+    current_id: AtomicUsize,
+    threads: Arc<RwLock<usize>>
 }
 
-impl<T, S> Scatter<T, S> {
-    pub fn new(_area: usize, function: fn(T)->S) -> Self { // new: provide function, arguments
-
-        let (tx, rx): (Sender<T>, Receiver<T>) = mpsc::channel();
-
-        Scatter { _area, function }
+impl<S: 'static + Send + std::marker::Sync, T: 'static + Send> Scatter<S, T> {
+    pub fn new(area: usize, function: fn(T)->S) -> Self { // new: provide function, arguments
+        Scatter { area, function, 
+            values: Arc::new(RwLock::new(HashMap::new())),
+            current_id: AtomicUsize::new(0),
+            threads: Arc::new(RwLock::new(0))
+        }
     } 
 
-    pub fn feed(&self, data: T) -> usize { // feed argument then scatter it
-
-
-        return 1
+    pub fn feed(&mut self, data: T) -> usize { // feed argument then scatter it
+        while self.threads.read().unwrap().ge(&self.area) { thread::sleep(Duration::from_millis(1)); }
+        
+        let id = self.current_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        let values = Arc::clone(&self.values);
+        
+        let fun = self.function;
+        self.threads.write().expect("RwLock poisoned").add_assign(1);
+        let threads = Arc::clone(&self.threads);
+        thread::spawn(move || {
+            let result = fun(data);
+            values.write().expect("RwLock poisoned").insert(id, result);
+            threads.write().expect("RwLock poisoned").sub_assign(1);
+        });
+        return id
     } 
 }
 
@@ -26,7 +44,7 @@ impl<T, S> Scatter<T, S> {
 
 #[test]
 fn run() {
-    let scatter = Scatter::new(5, |data: (&str, u16)| {
+    let mut scatter = Scatter::new(5, |data: (&str, u16)| {
         let (ip, port) = data;
         match std::net::TcpStream::connect_timeout(&format!("{}:{}", ip, port).parse().unwrap(), std::time::Duration::from_millis(1000)) {
             Ok(_) => true,
@@ -34,7 +52,17 @@ fn run() {
         }
     });
 
-    let id = scatter.feed(("92.204.50.150", 80));
-    println!("{}", id);
+    for _ in 0..20 {
+        let id = scatter.feed(("92.204.50.150", 80));
+        println!("{}", id);
+    }
+
+    println!("{:?}", scatter.values);
+
+    thread::sleep(Duration::from_secs(1));
+
+    println!("{:?}", scatter.values);
+
+    println!();
 }
 
