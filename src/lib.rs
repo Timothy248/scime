4a3df865f143c1cb52042fdedcd1121a19c47816
Timothy248 <tbenker@gmx.ch>
2022-05-21 16:45:33 +0200

    changed feed from locking to be kinda async

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,70 @@
 use std::ops::{AddAssign, SubAssign};
-use std::sync::atomic::AtomicUsize;
-use std::thread;
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
-use std::time::Duration;
+use std::{collections::HashMap, thread} ;
+use std::sync::{Arc, RwLock, Mutex, atomic::AtomicUsize};
 
 pub struct Scatter<S: 'static + Send, T: 'static + Send> {
     area: usize, // max thread count
-    function: fn(T) -> S,
+    function: Arc<fn(T) -> S>,
     pub values: Arc<RwLock<HashMap<usize, S>>>,
     current_id: AtomicUsize,
-    threads: Arc<RwLock<usize>>
+    eaters: Arc<RwLock<usize>>,
+    data: Arc<Mutex<Vec<(usize, T)>>>
 }
 
 impl<S: 'static + Send + std::marker::Sync, T: 'static + Send> Scatter<S, T> {
     pub fn new(area: usize, function: fn(T)->S) -> Self { // new: provide function, arguments
-        Scatter { area, function, 
+        Scatter { area, 
+            function: Arc::new(function), 
             values: Arc::new(RwLock::new(HashMap::new())),
             current_id: AtomicUsize::new(0),
-            threads: Arc::new(RwLock::new(0))
+            eaters: Arc::new(RwLock::new(0)),
+            data: Arc::new(Mutex::new(Vec::new()))
         }
-    } 
+    }
 
-    pub fn feed(&mut self, data: T) -> usize { // feed argument then scatter it
-        while self.active_threads().ge(&self.area) { thread::sleep(Duration::from_millis(1)); }
-        
-        let id = self.current_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+    // if active threads < max threads create new thread, thread gets data itself
+    // else thread on finish checks queue for new data 
+    //    if new data repeats cycle
+    //    else exits decreases counter of active threads
+
+    fn eat(&self) {
+        if self.get_eaters() < self.area { self.dispatch_eater() }
+    }
+
+    fn dispatch_eater(&self) {
+        let data = Arc::clone(&self.data);
         let values = Arc::clone(&self.values);
-        
-        let fun = self.function;
-        self.threads.write().expect("RwLock poisoned").add_assign(1);
-        let threads = Arc::clone(&self.threads);
+        let function = Arc::clone(&self.function);
+        let eaters = Arc::clone(&self.eaters);
+
+        self.eaters.write().unwrap().add_assign(1);
         thread::spawn(move || {
-            let result = fun(data);
-            values.write().expect("RwLock poisoned").insert(id, result);
-            threads.write().expect("RwLock poisoned").sub_assign(1);
+            let mut has_data = true;
+
+            while has_data {
+                let mut lock = data.lock().unwrap();
+
+                match lock.pop() {
+                    Some((cur_id, cur_data)) => {
+                        let result = function(cur_data);
+                        values.write().unwrap().insert(cur_id, result);
+                    },
+                    None => has_data = false
+                }
+                drop(lock);
+            }
+
+            eaters.write().unwrap().sub_assign(1);
         });
-        return id
     }
 
-    pub fn active_threads(&self) -> usize{ *self.threads.read().unwrap() }
-
-}
-
-// feed returns id once function is evaluated hashmap[id] contains value
-
-#[test]
-fn run() {
-    let mut scatter = Scatter::new(5, |data: (&str, u16)| {
-        let (ip, port) = data;
-        match std::net::TcpStream::connect_timeout(&format!("{}:{}", ip, port).parse().unwrap(), std::time::Duration::from_millis(1000)) {
-            Ok(_) => true,
-            Err(_) => false
-        }
-    });
-
-    for _ in 0..20 {
-        let id = scatter.feed(("92.204.50.150", 80));
-        println!("{}", id);
+    pub fn feed(&mut self, data: T) -> usize { // feed data get data processing id
+        let id = self.current_id.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        self.data.lock().unwrap().push((id, data));
+        self.eat();
+        return id;
     }
 
-    println!("{:?}", scatter.values);
-
-    thread::sleep(Duration::from_secs(1));
-
-    println!("{:?}", scatter.values);
-
-    println!();
+    pub fn get_eaters(&self) -> usize{ *self.eaters.read().unwrap() }
 }
 
